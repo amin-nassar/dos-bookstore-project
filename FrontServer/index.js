923169a47d1152f9c18508d265c010ff02a48c82
amin-nassar <amin.nassar@stu.najah.edu>
2021-11-04 18:46:29 +0200

    Changed Main IP Address

--- FrontServer/index.js
+++ FrontServer/index.js
@@ -8,7 +8,7 @@ const CATALOG_PORT = 3030;
 const ORDER_PORT = 5050;
 
 // Domain Name
-const MAIN_IP = "192.168.1.116";
+const MAIN_IP = "192.168.1.117";
 
 // Endpoints
 const CATALOG_ENDPOINT = `http://${MAIN_IP}:${CATALOG_PORT}`;
@@ -19,30 +19,30 @@ const app = express().use(express.json());
 
 // GET Book's Info By ID
 app.get("/info/:id", async (req, res) => {
-    const bookId = req.params.id;
-    const response = await fetch(`${CATALOG_ENDPOINT}/query-by-item/${bookId}`);
-    const body = await response.json();
-    res.status(response.status).json(body);
+  const bookId = req.params.id;
+  const response = await fetch(`${CATALOG_ENDPOINT}/query-by-item/${bookId}`);
+  const body = await response.json();
+  res.status(response.status).json(body);
 });
 
 // GET Books By Their Category
 app.get("/search/:category", async (req, res) => {
-    const cat = req.params.category;
-    const response = await fetch(`${CATALOG_ENDPOINT}/query-by-subject/${cat}`);
-    const body = await response.json();
-    res.status(200).json(body);
+  const cat = req.params.category;
+  const response = await fetch(`${CATALOG_ENDPOINT}/query-by-subject/${cat}`);
+  const body = await response.json();
+  res.status(200).json(body);
 });
 
 // PATCH Purchase Single Book
 app.patch("/purchase/:id", async (req, res) => {
-    const bookId = req.params.id;
-    const response = await fetch(`${ORDER_ENDPOINT}/purchase/${bookId}`);
-    const body = await response.json();
-    res.status(response.status).json(body);
+  const bookId = req.params.id;
+  const response = await fetch(`${ORDER_ENDPOINT}/purchase/${bookId}`);
+  const body = await response.json();
+  res.status(response.status).json(body);
 });
 
 // PORT Listening Callback Function
 const listenCallback = () =>
-    console.log(`Active On: http://${MAIN_IP}:${PORT_NUMBER}`);
+  console.log(`Active On: http://${MAIN_IP}:${PORT_NUMBER}`);
 
 app.listen(PORT_NUMBER, listenCallback);
